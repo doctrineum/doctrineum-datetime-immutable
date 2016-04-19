1d5f6a9f995e9b042dd9feef07664da2f16bfe62
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-04-19 12:46:22 +0200

    Self registering type logic extracted to abstract parent

--- Doctrineum/DateTimeImmutable/DateTimeImmutableType.php
+++ Doctrineum/DateTimeImmutable/DateTimeImmutableType.php
@@ -4,16 +4,12 @@ namespace Doctrineum\DateTimeImmutable;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\DateTimeType;
 use Doctrine\DBAL\Types\Type;
-use Granam\Tools\ValueDescriber;
-use Granam\Strict\Object\StrictObjectTrait;
 
 /**
- * @method static DateTimeImmutableType getType($name),
- * @see Type::getType
+ * @method static DateTimeImmutableType getType($name)
  */
-class DateTimeImmutableType extends Type
+class DateTimeImmutableType extends AbstractSelfRegisteringType
 {
-    use StrictObjectTrait;
 
     const DATETIME_IMMUTABLE = 'datetime_immutable';
 
@@ -23,53 +19,11 @@ class DateTimeImmutableType extends Type
     private $dateTimeType;
 
     /**
-     * @return bool If enum has not been registered before and was registered now
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    public static function registerSelf()
-    {
-        if (static::hasType(static::getTypeName())) {
-            static::checkRegisteredType();
-
-            return false;
-        }
-
-        static::addType(static::getTypeName(), get_called_class());
-
-        return true;
-    }
-
-    /**
-     * Gets the strongly recommended name of this type.
-     * Its used at @see \Doctrine\DBAL\Platforms\AbstractPlatform::getDoctrineTypeComment
-     *
-     * @return string
+     * @inheritdoc
      */
     public static function getTypeName()
     {
         return self::DATETIME_IMMUTABLE;
-    }
-
-    private static function checkRegisteredType()
-    {
-        $alreadyRegisteredType = static::getType(static::getTypeName());
-        if (get_class($alreadyRegisteredType) !== get_called_class()) {
-            throw new Exceptions\TypeNameOccupied(
-                'Under type of name ' . ValueDescriber::describe(static::getTypeName()) .
-                ' is already registered different type ' . get_class($alreadyRegisteredType)
-            );
-        }
-    }
-
-    /**
-     * Finds out if current type is already in registry
-     *
-     * @return bool
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    public static function isRegistered()
-    {
-        return static::hasType(static::getTypeName());
     }
 
     /**
@@ -84,7 +38,7 @@ class DateTimeImmutableType extends Type
      * @return DateTimeType
      * @throws \Doctrine\DBAL\DBALException
      */
-    private function getDateTimeType()
+    protected function getDateTimeType()
     {
         if ($this->dateTimeType === null) {
             $this->dateTimeType = parent::getType(Type::DATETIME);
@@ -125,17 +79,6 @@ class DateTimeImmutableType extends Type
         }
 
         return $val;
-    }
-
-    /**
-     * Gets the strongly recommended name of this type.
-     * Its used at @see \Doctrine\DBAL\Platforms\AbstractPlatform::getDoctrineTypeComment
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return static::getTypeName();
     }
 
 }
